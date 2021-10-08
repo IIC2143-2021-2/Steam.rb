12413c93e7fca914398d721a06201e995126f833
miortiz1 <miortiz1@uc.cl>
2021-10-07 23:26:37 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-
-require 'faker'
-
-@user= User.create(username: "Peje", email: "peje@uc.cl", phone: "18231414", age: 20, password: '123456', password_confirmation: '123456')
-@character1= Character.create(name: "harry", attack: Faker::Number.between(from: 1, to: 100), defense: Faker::Number.between(from: 1, to: 100), speed: Faker::Number.between(from: 1, to: 100), user: @user)
-@character2= Character.create(name: "ron", attack: Faker::Number.between(from: 1, to: 100), defense: Faker::Number.between(from: 1, to: 100), speed: Faker::Number.between(from: 1, to: 100), user: @user)
-@character3= Character.create(name: "hermione", attack: Faker::Number.between(from: 1, to: 100), defense: Faker::Number.between(from: 1, to: 100), speed: Faker::Number.between(from: 1, to: 100), user: @user)
-
-@game =Game.create(name: "quidditch", rating: Faker::Number.decimal(l_digits: 1, r_digits: 1), description: "empty", price: Faker::Number.between(from: 30, to: 80) , category: "", user: @user)
-
-GamesCharacter.create(game: @game, character: @character1)
-GamesCharacter.create(game: @game, character: @character2)
-GamesCharacter.create(game: @game, character: @character3)
+20.times do
+  password = Faker::Alphanumeric.alphanumeric(number: 10)
+  user = User.create(
+      username: Faker::Internet.username,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.cell_phone_in_e164,
+      age: Faker::Number.between(from: 15, to: 50),
+      password: password,
+      password_confirmation: password
+    )
+  3.times do
+    game = user.games.create(
+      name: Faker::Game.title,
+      rating: Faker::Number.decimal(l_digits: 1, r_digits: 1),
+      description: "No description",
+      price: Faker::Number.between(from: 30, to: 80),
+      category: Faker::Game.genre
+      )
+    2.times do
+      game.games_characters.create(
+     	  character: Character.create!(
+          name: Faker::Name.name,
+          attack: Faker::Number.between(from: 1, to: 100),
+          defense: Faker::Number.between(from: 1, to: 100),
+          speed: Faker::Number.between(from: 1, to: 100),
+          user: user
+        )
+      )
+    end
+  end
+end
